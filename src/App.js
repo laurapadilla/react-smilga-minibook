a077fa82674d8f4a01f2315cddb1c6e21859bc87
Laura Padilla <hi@lauracodes.com>
2021-01-18 14:34:40 -0500

    add book components

--- src/App.js
+++ src/App.js
@@ -11,7 +11,30 @@ function BookList() {
 
 // book component
 const Book = () => {
-  return <article>this is a book</article>;
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg"
+      alt="Book Cover for If Animals Kissed Goodnight "
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>If Animals Kissed Goodnight</h1>;
+};
+
+const Author = () => {
+  return <p>Ann Paul</p>;
 };
 
 // main app component
