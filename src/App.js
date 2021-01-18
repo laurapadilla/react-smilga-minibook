391abdd06399462ac6845202a1a7aac2c388c830
Laura Padilla <hi@lauracodes.com>
2021-01-18 14:28:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+// book list component
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
+
+// book component
+const Book = () => {
+  return <article>this is a book</article>;
+};
+
+// main app component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BookList />
     </div>
   );
 }
