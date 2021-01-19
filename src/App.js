7fbc78fad58bf9267ecc6520a73de6bd9cd7b13e
Laura Padilla <hi@lauracodes.com>
2021-01-18 20:44:48 -0500

    iterate over an array

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
 import "./base.css";
 import "./index.css";
 
+// books array
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+    title: "If Animals Kissed Goodnight",
+    author: "Ann Paul",
+    id: 1,
+  },
+
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81LYYbsEPeL._AC_UL200_SR200,200_.jpg",
+    title: "The Vanishing Half",
+    author: "Brit Bennett",
+    id: 2,
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81y4MVuGEvL._AC_UL200_SR200,200_.jpg",
+    title: "I am smart, I am Blessed, I can do anything!",
+    author: "Alissa Holder",
+    id: 3,
+  },
+];
+
 // BOOK LIST COMPONENT
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        {/* children props go in between the component tags */}
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          cum nisi ut minima voluptatem nam libero numquam placeat molestias
-          iste?
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {/* iterate over the books array/objects */}
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id}></Book>;
+      })}
     </section>
   );
 }
 
-// set up variables
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
-  title: "If Animals Kissed Goodnight",
-  author: "Ann Paul",
-};
-
-const secondBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81LYYbsEPeL._AC_UL200_SR200,200_.jpg",
-  title: "The Vanishing Half",
-  author: "Brit Bennett",
-};
-
 // BOOK COMPONENT - this will be passed down to the BookList component at the top
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
   // you can destructure each prop so you don't have to write props.title, etc
-  const { img, title, author, children } = props;
+  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="Book Cover for If Animals Kissed Goodnight " />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
