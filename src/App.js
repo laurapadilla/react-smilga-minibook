f8f5547c52acbb3ee5b19f770b2e29d392acc86f
Laura Padilla <hi@lauracodes.com>
2021-01-18 20:59:09 -0500

    click events examples

--- src/App.js
+++ src/App.js
@@ -43,11 +43,31 @@ function BookList() {
 const Book = ({ img, title, author }) => {
   // you can destructure each prop so you don't have to write props.title, etc
   // const { img, title, author } = props;
+
+  const clickHandler = () => {
+    alert("BOOM");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="Book Cover for If Animals Kissed Goodnight " />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference exmaple
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
